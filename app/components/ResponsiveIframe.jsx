3fc2bfb9721d16e86f797aa58e18b86c28e9dedc
Ravipatelbg <114903445+Ravipatelbg@users.noreply.github.com>
2023-06-20 14:54:50 +0530

    Push the code for set iframe height

--- app/components/ResponsiveIframe.jsx
+++ app/components/ResponsiveIframe.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useRef, useState} from 'react';
 
 export function ResponsiveIframe({url}) {
   return (
-    <div className="mt-[-50px] min-[992px]:mt-[0]">
+    <div className="md:mt-[-50px] min-[992px]:mt-[0]">
       <iframe
         src={url}
         title="Embedded Content"
@@ -12,7 +12,7 @@ export function ResponsiveIframe({url}) {
         id="iFrame1"
         width={'100%'}
         // style={{height: iframeHeight}}
-        className=' max-[991px]:h-[calc(100vh_-_72px)] min-[992px]:h-[1260px] min-[1200px]:h-[1310px] min-[1400px]:h-[1110px]'
+        className=' max-[991px]:h-screen min-[992px]:h-[1040px] min-[1179px]:h-[1039px] min-[1800px]:h-[1420px]'
       />
     </div>
   );
